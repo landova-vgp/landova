82c2b7242708c4788e47f09fa2954f2b871af536
Landova <landova.vgp.projects@gmail.com>
2025-07-08 17:11:38 +0530

    Hamburger for nav links in mobile view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import logo from './assets/logo.png';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
   return (
     <div className="homepage">
       <img src={logo} alt="Logo" className="logo" />
@@ -11,10 +18,16 @@ function App() {
         <p>70 Years of Trust. Generations of Happy Homeowners.</p>
       </div>
 
-      <div className="nav-links">
+      <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
         <a href="#">About Us</a>
         <a href="#">Projects</a>
         <a href="#">Contact</a>
+      </div>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar" />
+        <div className="bar" />
+        <div className="bar" />
       </div>
     </div>
   );
