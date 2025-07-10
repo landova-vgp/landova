9f619a6651d9bb3d0e73b63c2afca9d3b9460cea
Landova <landova.vgp.projects@gmail.com>
2025-07-10 22:57:24 +0530

    router issue in Github pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import ProjectDetail from './components/ProjectDetail';
-import ContactUs from './components/ContactUs';
 
 function App() {
    return (
-    <Router>
+    <BrowserRouter basename="/landova">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
