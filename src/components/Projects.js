4f6bcabf18bc975681e91c6ab1034ed39033fcf8
Landova <landova.vgp.projects@gmail.com>
2025-07-10 16:38:02 +0530

    Appearance changed to Refuel page look

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,15 +10,20 @@ function Projects() {
           <div className="project-image">
             <img src={projectImage} alt="Project Thumbnail" />
           </div>
-          <h2 className="project-title">
-            <small>No.001</small> In Between OMR – GST – onto Chengalpattu – Thiruporur Road
-          </h2>
-          <p className="project-desc">Kottamedu Junction Keelu</p>
-          <i className="project-arrow icon">
-            <svg width="9" height="13" viewBox="0 0 9 13">
-                <use href="#svg-next" />
-            </svg>
-          </i>
+
+          <div className="project-details">
+            <h2 className="project-title">
+              <small>No.001</small> - In Between OMR – GST – onto Chengalpattu – Thiruporur Road
+            </h2>
+
+            <p className="project-desc">Kottamedu Junction Keelu</p>
+
+            <i className="project-arrow icon">
+              <svg width="9" height="13" viewBox="0 0 9 13">
+                  <use href="#svg-next" />
+              </svg>
+            </i>
+          </div>
         </a>
       </article>
     </div>
